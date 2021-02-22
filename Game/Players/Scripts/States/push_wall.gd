fe87235b27aab0ed415397d0ab780d72e9b4ceff
JonathanPicques <jonathan.picques@gmail.com>
2021-02-22 23:29:23 +0100

    fix(player): fix push wall

--- Game/Players/Scripts/States/push_wall.gd
+++ Game/Players/Scripts/States/push_wall.gd
@@ -5,6 +5,7 @@ func start_state():
 
 func process_state(delta: float):
 	player_node.handle_gravity(delta, player_node.GRAVITY_MAX_SPEED, player_node.GRAVITY_ACCELERATION)
+	player_node.handle_floor_move(delta, player_node.RUN_MAX_SPEED, player_node.RUN_ACCELERATION, player_node.RUN_DECELERATION)
 	if not player_node.is_on_floor():
 		return player_node.fsm.state_nodes.fall
 	if player_node.input_jump_once and player_node.is_able_to_jump():
